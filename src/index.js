cfcadce571f0f20cc878e54cf45f3a32b14746b4
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-30 14:19:57 -0700

    Add meshprovider to app context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MeshProvider from "./providers/MeshProvider";
+import App from "./App";
 import "./index.scss";
-import { App } from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MeshProvider>
+      <App />
+    </MeshProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
