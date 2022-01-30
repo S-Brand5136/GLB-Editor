906861b3a861110b8ce8dbddf4a3cf7635aab97f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-30 14:19:13 -0700

    Add Scenegraaph, viewer, btngroup to app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import SceneGraph from "./components/SceneGraph";
+import Viewer from "./components/Viewer";
+import Button from "./components/Button";
+
+import "./App.scss";
+
+const App = () => {
+  const [show, setShow] = useState(true);
+
+  const clickHandler = (view) => {
+    if (view === "viewer") {
+      return setShow(false);
+    }
+
+    setShow(true);
+  };
+
+  return (
+    <div className='App'>
+      <SceneGraph show={show}></SceneGraph>
+      <Viewer show={!show}></Viewer>
+
+      <div className='btn-group'>
+        <Button
+          viewSelect
+          selected={!show}
+          onClick={() => clickHandler("viewer")}
+        >
+          <img
+            src={process.env.PUBLIC_URL + `/assets/icons8-view-64.png`}
+            alt='viewer'
+          />
+        </Button>
+        <Button
+          viewSelect
+          selected={show}
+          onClick={() => clickHandler("graph")}
+        >
+          <img
+            src={process.env.PUBLIC_URL + `/assets/icons8-graph-64.png`}
+            alt='graph'
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
