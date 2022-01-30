8e8736ff9b659bc55b9b7da8635dadacd09757d8
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-30 14:20:21 -0700

    Create Button component and styling

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import classNames from "classnames";
+import "./styles/Button.scss";
+
+const Button = (props) => {
+  let buttonClass = classNames("btn", {
+    "btn--submit": props.submit,
+    "btn--viewSelect": props.viewSelect,
+    "btn--selected": props.selected,
+  });
+
+  return (
+    <button className={buttonClass} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
