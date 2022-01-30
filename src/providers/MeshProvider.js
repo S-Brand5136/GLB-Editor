5b04b4aec0be19b94d1148beb61a5e0bc41a351b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-30 14:20:06 -0700

    Create MeshProvider

---
+++ src/providers/MeshProvider.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export default function MeshProvider(props) {
+  const [mesh, setMesh] = useState(null);
+
+  const addMesh = (mesh) => {
+    setMesh(mesh);
+  };
+
+  const meshData = {
+    mesh,
+    addMesh,
+  };
+
+  return (
+    <meshContext.Provider value={meshData}>
+      {props.children}
+    </meshContext.Provider>
+  );
+}
+
+export const meshContext = createContext();
