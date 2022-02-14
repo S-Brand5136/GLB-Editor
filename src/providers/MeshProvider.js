f36805c3a7c2fa163de4ee479ed4a9ccab9e9b39
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-13 22:00:26 -0700

    Add graph to app context

--- src/providers/MeshProvider.js
+++ src/providers/MeshProvider.js
@@ -2,14 +2,21 @@ import { createContext, useState } from "react";
 
 export default function MeshProvider(props) {
   const [mesh, setMesh] = useState(null);
+  const [graph, setGraph] = useState(null);
 
   const addMesh = (mesh) => {
     setMesh(mesh);
   };
 
+  const addGraph = (treeGraph) => {
+    setGraph(treeGraph);
+  };
+
   const meshData = {
     mesh,
     addMesh,
+    addGraph,
+    graph,
   };
 
   return (
