6623773677e5c0613fbea80300d6fa0d8ee20609
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-13 21:58:59 -0700

    Add function to create treegraph

--- src/helpers/createGraph.js
+++ src/helpers/createGraph.js
@@ -1,1 +1,15 @@
-export const createGraph = () => {};
+import { Tree } from "../scripts/Tree";
+
+export const createGraph = (scene) => {
+  let treeGraph;
+
+  scene.traverse((child) => {
+    if (child.parent === null) {
+      treeGraph = new Tree(child.id, child);
+    } else {
+      treeGraph.insert(child.parent.id, child.id, child);
+    }
+  });
+
+  return treeGraph;
+};
