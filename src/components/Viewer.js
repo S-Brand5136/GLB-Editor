8b6ec05b7c52d4dd27965b3b895bd4baf3c81bef
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-13 19:06:28 -0700

    Add base code for viewer

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,15 +1,34 @@
+import { useEffect, useContext, useState } from "react";
 import classNames from "classnames";
 import { Canvas } from "@react-three/fiber";
+import { meshContext } from "../providers/MeshProvider";
 
 import "./styles/Viewer.scss";
+import { OrbitControls } from "@react-three/drei";
 
 const Viewer = (props) => {
-  let canvasClass = classNames("canvas", {
-    "canvas--hide": !props.show,
-    "canvas--show": props.show,
+  const [show, setShow] = useState(false);
+  let viewerClass = classNames("viewer", {
+    "viewer--hide": !props.show,
+    "viewer--show": props.show,
   });
 
-  return <Canvas className={canvasClass}>{props.children}</Canvas>;
+  const { mesh } = useContext(meshContext);
+
+  useEffect(() => {
+    if (mesh) {
+      setShow(true);
+    }
+  }, [mesh]);
+
+  return (
+    <Canvas className={viewerClass}>
+      <OrbitControls />
+      <ambientLight intensity={1} />
+      <directionalLight color='white' position={[-2, -3, 5]} />
+      {show ? <primitive object={mesh} /> : <></>}
+    </Canvas>
+  );
 };
 
 export default Viewer;
