a3353c30e2fe67e3e06b9fe3648cfd935b81212b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-13 17:21:08 -0700

    Add base code to load in GLB

--- src/components/MeshLoader.js
+++ src/components/MeshLoader.js
@@ -1,13 +1,73 @@
-import Button from "./Button";
+import { useState, useContext, useEffect } from "react";
+import { meshContext } from "../providers/MeshProvider";
+import { SimpleDropzone } from "simple-dropzone";
+import { LoaderUtils } from "three";
 import Input from "./Input";
 
 import "./styles/MeshLoader.scss";
+import { loadAsset } from "../helpers/loadAsset";
 
 const MeshLoader = () => {
+  let inputEl, dropZone;
+  const [error, setError] = useState(null);
+
+  const { addMesh } = useContext(meshContext);
+
+  // Sets up drag and drop controller
+  useEffect(() => {
+    dropZone = document.querySelector(".upload-form");
+    inputEl = document.querySelector("#asset");
+    const dropCtrl = new SimpleDropzone(dropZone, inputEl);
+    dropCtrl.on("drop", ({ files }) => SubmitHandler(files));
+    dropCtrl.on("dropstart", () => {});
+    dropCtrl.on("droperror", () => {});
+  }, []);
+
+  const SubmitHandler = async (fileMap) => {
+    let rootFile;
+    let rootPath;
+    Array.from(fileMap).forEach(([path, file]) => {
+      if (file.name.match(/\.(gltf|glb)$/)) {
+        rootFile = file;
+        rootPath = path.replace(file.name, "");
+      }
+    });
+
+    if (!rootFile) {
+      setError("No GLB or GLTF file found.");
+    }
+
+    const fileURL =
+      typeof rootFile === "string" ? rootFile : URL.createObjectURL(rootFile);
+
+    const baseURL = LoaderUtils.extractUrlBase(fileURL);
+
+    loadAsset(baseURL, rootPath, fileMap, fileURL)
+      .then((data) => {
+        addMesh(data.scene);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      });
+  };
+
   return (
     <div className='meshLoader'>
-      <Input type='file' id='asset' name='3D-Asset' asset />
-      <Button submit>Submit</Button>
+      <div className='upload-form'>
+        <h3>Drag & Drop or Click to upload file</h3>
+        <Input
+          onClick={() => {
+            setError(null);
+          }}
+          type='file'
+          id='asset'
+          name='3D-Asset'
+          asset
+        />
+        {error ? <p>{error}</p> : <p></p>}
+      </div>
     </div>
   );
 };
