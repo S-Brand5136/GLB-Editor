de46f71426fdb38a751f76dc955c0002449b00e0
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-14 19:19:33 -0700

    Get graph from context, add TreeGraph component to if clause

--- src/components/SceneGraph.js
+++ src/components/SceneGraph.js
@@ -4,6 +4,7 @@ import { meshContext } from "../providers/MeshProvider";
 import MeshLoader from "./MeshLoader";
 
 import "./styles/SceneGraph.scss";
+import TreeGraph from "./TreeGraph";
 
 const SceneGraph = (props) => {
   let graphClass = classNames("graph", {
@@ -12,7 +13,7 @@ const SceneGraph = (props) => {
   });
 
   const [show, setShow] = useState(false);
-  const { mesh } = useContext(meshContext);
+  const { mesh, graph } = useContext(meshContext);
 
   useEffect(() => {
     if (mesh) {
@@ -24,7 +25,7 @@ const SceneGraph = (props) => {
 
   return (
     <main className={graphClass}>
-      {show ? <MeshLoader></MeshLoader> : <h1>GLB LOADED</h1>}
+      {show ? <MeshLoader></MeshLoader> : <TreeGraph graph={graph}></TreeGraph>}
     </main>
   );
 };
