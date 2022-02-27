a3b6e2edd5f2a4bde1bc0330c1ddd1a566d1dea4
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-27 15:47:03 -0700

    Add OptionsProvider context, button onClick

--- src/components/optionsMenu/OptionsSelector.js
+++ src/components/optionsMenu/OptionsSelector.js
@@ -1,14 +1,24 @@
-import React from "react";
+import { useContext } from "react";
 import Button from "../Button";
+import "./styles/OptionsSelector.scss";
+
+import { optionsContext } from "../../providers/OptionsProvider";
 
 const OptionsSelector = (props) => {
+  const { selectedOption, updateSelectedOption } = useContext(optionsContext);
+
   const optionSelection = props.options.map((option, index) => (
-    <Button key={index} option onClick={() => console.log("clicked")}>
+    <Button
+      key={index}
+      option
+      option_selected={option === selectedOption}
+      onClick={() => updateSelectedOption(option)}
+    >
       {option}
     </Button>
   ));
 
-  return <header>{optionSelection}</header>;
+  return <header className='options-selector'>{optionSelection}</header>;
 };
 
 export default OptionsSelector;
