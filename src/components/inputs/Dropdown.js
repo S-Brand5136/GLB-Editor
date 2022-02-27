53de59c10f5dbd80250ca352d9cbc52af02e4e40
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-26 21:12:23 -0700

    Create Dropdown input

---
+++ src/components/inputs/Dropdown.js
@@ -0,0 +1,9 @@
+import "./styles/Dropdown.scss";
+
+const Dropdown = ({ array, onChange }) => {
+  const optionList = array.map((item) => <option value={item}>{item}</option>);
+
+  return <selection onChange={onChange}>{optionList}</selection>;
+};
+
+export default Dropdown;
