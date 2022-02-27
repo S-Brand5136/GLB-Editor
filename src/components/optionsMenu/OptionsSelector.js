9f8aee6ab7c58f5437947d01aef97ecd78329b44
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-02-26 22:08:53 -0700

    Add base optionSelector JSX/Styling

--- src/components/optionsMenu/OptionsSelector.js
+++ src/components/optionsMenu/OptionsSelector.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "../Button";
 
-const OptionsSelector = () => {
-  return <div>OptionsSelector</div>;
+const OptionsSelector = (props) => {
+  const optionSelection = props.options.map((option, index) => (
+    <Button key={index} option onClick={() => console.log("clicked")}>
+      {option}
+    </Button>
+  ));
+
+  return <header>{optionSelection}</header>;
 };
 
 export default OptionsSelector;
