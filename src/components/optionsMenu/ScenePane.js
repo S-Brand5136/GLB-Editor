94414cb3b1d6bda4a19c8f54a5259524759bf0b1
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-03-13 18:11:54 -0600

    Fix Import statements, add globals style sheet

--- src/components/optionsMenu/ScenePane.js
+++ src/components/optionsMenu/ScenePane.js
@@ -1,10 +1,20 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { meshContext } from "../../providers/MeshProvider";
 import EmptyScene from "./EmptyScene";
 import "./styles/ScenePane.scss";
 
 const ScenePane = () => {
+  const [elementNames, setElementNames] = useState(null);
   const { mesh, graph } = useContext(meshContext);
+
+  useEffect(() => {
+    if (mesh) {
+      console.log(graph);
+      //   mesh.traverse((child) => {
+      //     console.log(child);
+      //   });
+    }
+  }, [graph]);
 
   return (
     <section className='scene-pane'>{!mesh ? <EmptyScene /> : <></>}</section>
