4af87d4b0b996a9ce1dbd5cd72a002dc622ea6ec
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2022-10-23 19:53:40 -0600

    Add Hook for drag inputs

---
+++ src/hooks/use-drag-input.hook.js
@@ -0,0 +1,42 @@
+import { useState, useCallback, useEffect } from "react";
+
+export function useDragInput(value, setValue) {
+  const [snapshot, setSnapshot] = useState(value);
+  const [startVal, setStartVal] = useState(0);
+
+  // Start the drag to change operation when the mouse button is down.
+  const onStart = useCallback(
+    (event) => {
+      setStartVal(event.clientX);
+      setSnapshot(value);
+    },
+    [value]
+  );
+
+  useEffect(() => {
+    // Only change the value if the drag was actually started.
+    const onUpdate = (event) => {
+      if (startVal) {
+        if (event.layerX < 0) {
+          return setValue(value - 0.01);
+        }
+
+        setValue(value + 0.01);
+      }
+    };
+
+    // Stop the drag operation now.
+    const onEnd = () => {
+      setStartVal(0);
+    };
+
+    document.addEventListener("mousemove", onUpdate);
+    document.addEventListener("mouseup", onEnd);
+    return () => {
+      document.removeEventListener("mousemove", onUpdate);
+      document.removeEventListener("mouseup", onEnd);
+    };
+  }, [startVal, setValue, snapshot, value]);
+
+  return onStart;
+}
